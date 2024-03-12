646d95be9b877c677c7868f0b7cdc255c87dc597
pranav.ahire <pranav.ahire@laminaar.in>
2024-03-12 13:06:36 +0530

    Fix the view of todo list

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -7,10 +7,10 @@ import { useEffect, useState } from "react";
 const Todo = () => {
     console.log("Rendering /Todos");
     const api = useSelector(s => s.todo.todos);
-    
+
     const data = [...api]
     data.sort((a, b) => a.completed - b.completed)
-    
+
     const [input, setInput] = useState({ title: "", desc: "", completed: false })
     const [error, setError] = useState({ title: "" })
     const dispatch = useDispatch()
@@ -20,7 +20,7 @@ const Todo = () => {
         !input.desc ? error.desc = "Description shoould not be empty" : null
         setError(error)
     }
-    
+
     useEffect(() => {
 
         if (!Object.keys(error).length) {
@@ -28,42 +28,45 @@ const Todo = () => {
             setInput({ ...input, title: "", desc: "" })
         }
     }, [error])
-    
+
     const handlechange = (e) => setInput({ ...input, [e.target.name]: e.target.value })
 
 
     return (
         <>
-            <div className="flex flex-col items-center">
-                <div>
-                    <label>Title</label>
-                    <input
-                        className="border p-1 m-2 border-2px bg-slate-300"
-                        type="text"
-                        name="title"
-                        placeholder="Title"
-                        value={input.title}
-                        onChange={handlechange}
-                    ></input>
-                </div>
-                {error.title ? <p className="text-red-500">{error.title}</p> : null}
-                <div className="flex items-center">
-                    <label>desc</label>
-                    <textarea
-                        name="desc"
-                        rows={4}
-                        value={input.desc}
-                        onChange={handlechange}
-                        className=' border border-solid m-2 p-2 h-10  '
-                        placeholder="Description">
-                    </textarea>
+            <div className="flex flex-col  items-center">
+                <div className="flex flex-col w-72 ">
+                    <div className="flex justify-between items-center" >
+                        <label >Title</label>
+                        <input
+                            className="border p-1 m-2 border-2px bg-slate-300"
+                            type="text"
+                            name="title"
+                            placeholder="Title"
+                            value={input.title}
+                            onChange={handlechange}
+                        ></input>
+                    </div>
+                    {error.title ? <p className="text-red-500">{error.title}</p> : null}
+                    <div className="flex justify-between items-center">
+                        <label className="">Description</label>
+                        <textarea
+                            name="desc"
+                            rows={6}
+                            
+                            value={input.desc}
+                            onChange={handlechange}
+                            className=' border border-solid m-2 p-2 h-10 w-48 '
+                            placeholder="Description">
+                        </textarea>
+                    </div>
                 </div>
                 {error.desc && <p className="text-red-500">{error.desc}</p>}
                 <button className="bg-blue-400 m-2 p-2 rounded-xl" onClick={handleClick}>CreateTask</button>
             </div>
             <div className="flex flex-col items-center">
 
-                <h1>RUN</h1>
+                <h1 className="text-4xl p-8">Task List - 📋</h1>
                 {
                     data.map(e => <div key={e.id} ><TodoList task={e}></TodoList></div>)
                 }
